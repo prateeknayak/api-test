df7c7bfc5646d1f6a1e47e657fc19d90a54a7b3b
Prateek Nayak <6571322+prateeknayak@users.noreply.github.com>
2019-06-12 08:46:06 +1000

    Adds an echo statement

--- bin/docker_build.sh
+++ bin/docker_build.sh
@@ -3,7 +3,7 @@
 if [ -z ${TRAVIS_TAG+x} ]; then
     docker build . -t pnyak/api-test:latest --build-arg app_version=latest
 else
-    echo "travis tag"
+    echo "tavis tag set"
     docker build . -t pnyak/api-test:${TRAVIS_TAG} --build-arg app_version=${TRAVIS_TAG}
 fi
 
