b4daf639b7027130babbd106f95ef7805c2d30ed
Prateek Nayak <6571322+prateeknayak@users.noreply.github.com>
2019-06-12 09:03:27 +1000

    Fixes scripts

--- bin/docker_build.sh
+++ bin/docker_build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "travis tag ${TRAVIS_TAG}:"
-if [ -z ${TRAVIS_TAG+x} ]; then
+if [[ -z "${TRAVIS_TAG}" ]]; then
     docker build . -t pnyak/api-test:latest --build-arg app_version=latest
 else
     echo "tavis tag set"
--- bin/docker_push.sh
+++ bin/docker_push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-if [ -z ${TRAVIS_TAG+x} ]; then
+if [[ -z "${TRAVIS_TAG}" ]]; then
     docker push pnyak/api-test:latest
 else
     echo "travis tag set"
