84082152ae85b578c78eee3bb8fe4ffe7718047c
Prateek Nayak <6571322+prateeknayak@users.noreply.github.com>
2019-06-12 08:32:17 +1000

    Tests travis tag

--- bin/docker_build.sh
+++ bin/docker_build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+echo "travis tag set: ${TRAVIS_TAG}"
 if [ -z ${TRAVIS_TAG+x} ]; then
+    echo "travis tag"
     docker build . -t pnyak/api-test:${TRAVIS_TAG} --build-arg app_version=${TRAVIS_TAG}
 fi
 
--- bin/docker_push.sh
+++ bin/docker_push.sh
@@ -2,6 +2,7 @@
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 if [ -z ${TRAVIS_TAG+x} ]; then
+    echo "travis tag set"
     docker push pnyak/api-test:${TRAVIS_TAG}
 fi
 
